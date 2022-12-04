36c76a99d58d54188f2d94cd6aabb3ea254daaa2
ImranHossain5722 <imd572258@gmail.com>
2022-12-04 16:32:51 +0600

    build nav bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,29 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            nave
+        <div className=''>
+            <div className='bg-white flex flex-col md:flex-row md:justify-start justify-center items-center'>
+                <div className='logo-container'>
+
+                    <Image width={150} height={50} src="/logo.png" alt='logo' />
+                </div>
+                <div className='nav ' >
+                    <ul className='text-black flex space-x-3  '>
+                        <Link href={'/'}> <li>Home</li>  </Link>
+                        <Link href={'/'}> <li>T-shirts</li> </Link>
+                        <Link href={'/'} > <li>Hoodies</li> </Link>
+                        <Link href={'/'} > <li>About Us</li> </Link>
+                        <Link href={'/'}>  <li>Contact us</li> </Link>
+                    </ul>
+                </div>
+                <div className='cart absolute  right-0 mx-5'>
+                    <button className='text-black'>cart</button>
+
+                </div>
+            </div>
         </div>
     );
 };
