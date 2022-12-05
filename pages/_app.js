b9b3670436fe26cd13c5573296af98fd6d83e384
ImranHossain5722 <imd572258@gmail.com>
2022-12-06 00:36:16 +0600

    build cart function

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,53 @@
+import { useEffect, useState } from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+ const [cart, setCart] =useState({})
+ const [subTotal, setSubTotal] = useState(0)
+  const saveCart =(myCart)=>{
+    localStorage.setItem("cart", myCart)
+  }
+
+  useEffect(()=>{
+
+
+  },[])
+
+const  addCart =(itemCode, qty, price, name, size, variant)=>{
+
+  let newCart =cart ;
+  if (itemCode in cart){
+    newCart[itemCode].qty = cart[itemCode].qty + qty
+
+  }else{
+
+newCart[itemCode]={qty:1, price, name , size, variant}
+  }
+  setCart(newCart)
+  saveCart(newCart)
+}
+
+const  removeFromCart =(itemCode, qty, price, name, size, variant)=>{
+
+  let newCart =cart ;
+  if (itemCode in cart){
+    newCart[itemCode].qty = cart[itemCode].qty - qty
+
+  }
+   if(newCart[itemCode]['qty']<=0){
+    delete newCart[itemCode]
+
+   }
+  setCart(newCart)
+  saveCart(newCart)
+}
+
+const clearCart =()=>{
+  setCart({})
+  saveCart({})
+}
 
   return <>
   <Navbar />
--- pages/hoodies.js
+++ pages/hoodies.js
@@ -1,9 +1,113 @@
+import Link from 'next/link';
 import React from 'react';
 
 const hoodies = () => {
     return (
         <div>
-            Hoodies
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap mx-auto">
+                        <div className="lg:w-1/5  md:w-1/2  w-full shadow-lg m-3">
+                            <Link href={'/products/the-code'}>
+                                <a className="block relative  rounded overflow-hidden">
+                                    <img alt="ecommerce" className=" m-auto    h-[30vh] md:h-[35vh]   blcok" src="https://m.media-amazon.com/images/I/71iAcOeGpJL._AC_UL320_.jpg" /> </a>
+                                <div className="mt-4 p-4">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Your choice hoodies</h2>
+                                    <p className="mt-1">$16.00</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="lg:w-1/5  md:w-1/2  w-full shadow-lg m-3">
+                            <Link href={'/products/the-code'}>
+                                <a className="block relative  rounded overflow-hidden">
+                                    <img alt="ecommerce" className=" m-auto    h-[30vh] md:h-[35vh]   blcok" src="https://m.media-amazon.com/images/I/71iAcOeGpJL._AC_UL320_.jpg" /> </a>
+                                <div className="mt-4 p-4">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Your choice hoodies</h2>
+                                    <p className="mt-1">$16.00</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="lg:w-1/5  md:w-1/2  w-full shadow-lg m-3">
+                            <Link href={'/products/the-code'}>
+                                <a className="block relative  rounded overflow-hidden">
+                                    <img alt="ecommerce" className=" m-auto    h-[30vh] md:h-[35vh]   blcok" src="https://m.media-amazon.com/images/I/71iAcOeGpJL._AC_UL320_.jpg" /> </a>
+                                <div className="mt-4 p-4">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Your choice hoodies</h2>
+                                    <p className="mt-1">$16.00</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="lg:w-1/5  md:w-1/2  w-full shadow-lg m-3">
+                            <Link href={'/products/the-code'}>
+                                <a className="block relative  rounded overflow-hidden">
+                                    <img alt="ecommerce" className=" m-auto    h-[30vh] md:h-[35vh]   blcok" src="https://m.media-amazon.com/images/I/71iAcOeGpJL._AC_UL320_.jpg" /> </a>
+                                <div className="mt-4 p-4">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Your choice hoodies</h2>
+                                    <p className="mt-1">$16.00</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="lg:w-1/5  md:w-1/2  w-full shadow-lg m-3">
+                            <Link href={'/products/the-code'}>
+                                <a className="block relative  rounded overflow-hidden">
+                                    <img alt="ecommerce" className=" m-auto    h-[30vh] md:h-[35vh]   blcok" src="https://m.media-amazon.com/images/I/71iAcOeGpJL._AC_UL320_.jpg" /> </a>
+                                <div className="mt-4 p-4">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Your choice hoodies</h2>
+                                    <p className="mt-1">$16.00</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="lg:w-1/5  md:w-1/2  w-full shadow-lg m-3">
+                            <Link href={'/products/the-code'}>
+                                <a className="block relative  rounded overflow-hidden">
+                                    <img alt="ecommerce" className=" m-auto    h-[30vh] md:h-[35vh]   blcok" src="https://m.media-amazon.com/images/I/71iAcOeGpJL._AC_UL320_.jpg" /> </a>
+                                <div className="mt-4 p-4">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Your choice hoodies</h2>
+                                    <p className="mt-1">$16.00</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="lg:w-1/5  md:w-1/2  w-full shadow-lg m-3">
+                            <Link href={'/products/the-code'}>
+                                <a className="block relative  rounded overflow-hidden">
+                                    <img alt="ecommerce" className=" m-auto    h-[30vh] md:h-[35vh]   blcok" src="https://m.media-amazon.com/images/I/71iAcOeGpJL._AC_UL320_.jpg" /> </a>
+                                <div className="mt-4 p-4">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Your choice hoodies</h2>
+                                    <p className="mt-1">$16.00</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="lg:w-1/5  md:w-1/2  w-full shadow-lg m-3">
+                            <Link href={'/products/the-code'}>
+                                <a className="block relative  rounded overflow-hidden">
+                                    <img alt="ecommerce" className=" m-auto    h-[30vh] md:h-[35vh]   blcok" src="https://m.media-amazon.com/images/I/71iAcOeGpJL._AC_UL320_.jpg" /> </a>
+                                <div className="mt-4 p-4">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Hoodies</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Your choice hoodies</h2>
+                                    <p className="mt-1">$16.00</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </Link>
+                        </div>
+                        
+                    </div>
+                   
+                </div>
+            </section>
         </div>
     );
 };
