cf697a704eb6c25cf580749311097f4a1f0db787
ImranHossain5722 <imd572258@gmail.com>
2022-12-15 03:36:47 +0600

    create backend api and connected monogodb database

---
+++ models/Order.js
@@ -0,0 +1,17 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+const OderSchema = new mongoose.Schema({
+ userId: {type: String, required: true},
+ products:[
+    {
+        productId:{type: String},
+        quantity:{type: Number,default:1}
+    }],
+    address:{type:String, required: true },
+    amount:{type: Number, required: true }, 
+    status:{type: String, default: 'Pending' , required: true } 
+
+},{timestamps:true});
+mongoose.models ={}
+export default mongoose.model("Order", OderSchema)
