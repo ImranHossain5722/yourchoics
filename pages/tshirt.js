5f2d4ebe9954b732834c8c1c253cfd87fe541dad
ImranHossain5722 <imd572258@gmail.com>
2022-12-17 04:22:29 +0600

    build color and size varient

--- pages/tshirt.js
+++ pages/tshirt.js
@@ -12,29 +12,41 @@ const tshirt = ({ products }) => {
         <div className="container px-5 py-24 mx-auto">
           <div className="">
             <div className=" flex flex-wrap mx-auto justify-center ">
-              {products.map((item) => {
+              {Object.keys(products).map((item) => {
                 return (
                   <Link
-                    key={item._id}
-                    href={`/products/${item.id}`}
+                    key={products[item]._id}
+                    href={`/products/${products[item].id}`}
                     className="lg:w-1/5  md:w-1/2  w-full shadow-lg m-3"
                   >
                     <a className="block relative  rounded overflow-hidden">
                       <img alt="ecommerce"
                         className="m-auto h-[30vh] md:h-[35vh] blcok"
-                        src={item.img} />
+                        src={products[item].img} />
                     </a>
                     <div className="mt-4 p-4">
                       <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1 capitalize">
-                        {item.category}
+                        {products[item].category}
                       </h3>
                       <h2 className="text-gray-900 title-font text-lg font-medium">
         
-                        {item.title}
+                        {products[item].title}
                       </h2>
-                      <p className="mt-1">${item.price}</p>
-                      <p className="mt-1">{item.size}</p>
-                      <p className="mt-1">{item.color}</p>
+                      <p className="mt-1">${products[item].price}</p>
+                      <div className="mt-1">
+                        {products[item].size.includes("s")&& <span>S,</span>}
+                        {products[item].size.includes("m")&& <span>M,</span>}
+                        {products[item].size.includes("l")&& <span>L,</span>}
+                        {products[item].size.includes("xl")&& <span>XL,</span>}
+                        {products[item].size.includes("xxl")&& <span>XXL,</span>}
+                      </div>
+                      <div className="mt-1">
+                      {products[item].color.includes("gray")&& <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes("blue")&& <button className="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes("red")&& <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes("black")&& <button className="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>}
+                      </div>
+                      
                       
                     </div>
                   </Link>
@@ -54,8 +66,31 @@ export async function getServerSideProps(context) {
     await mongoose.connect(process.env.MONGO_URI);
   }
   let products = await Product.find({category:"t-shirt"});
+  let tshirts ={}
+
+  for(let item of products){
+      if(item.title in tshirts){
+         if(!tshirts[item.title].color.includes(item.color) &&        
+          item.availableQty > 0){
+              tshirts[item.title].color.push(item.color)
+         } 
+         if(!tshirts[item.title].size.includes(item.size) &&        
+          item.availableQty>0){
+              tshirts[item.title].size.push(item.size)
+         } 
+      }
+      else{
+
+          tshirts[item.title] = JSON.parse(JSON.stringify(item))
+          if(item.availableQty > 0 ){
+              tshirts[item.title].color =[item.color]
+              tshirts[item.title].size =[item.size]
+          }
+      }
+  }  
+
   return {
-    props: { products: JSON.parse(JSON.stringify(products)) }, // will be passed to the page component as props
+    props: { products: JSON.parse(JSON.stringify(tshirts)) }, // will be passed to the page component as props
   };
 }
 export default tshirt;
