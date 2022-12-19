12ad81c01d66d0ea59a1e7c330cccce31ddcd384
ImranHossain5722 <imd572258@gmail.com>
2022-12-19 20:19:39 +0600

    make password enrypt login and signup

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,15 +1,17 @@
 import User from "../../models/User"
 import connectDb from "../../middleware/mongoose";
+var CryptoJS = require("crypto-js");
 
 
 const  handler = async (req,res)=>{
 
     if(req.method=="POST"){
         let user = await User.findOne({"email":req.body.email})
-
+        const bytes  = CryptoJS.AES.decrypt(user.password, 'secret123');
+        let decryptPass=bytes.toString(CryptoJS.enc.Utf8)
         if(user){
 
-            if(req.body.email== user.email && req.body.password== user.password){
+            if(req.body.email==user.email && req.body.password== decryptPass){
                 res.status(200).json({success: true, email:user.email, name:user.name})
             
             }
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,12 +1,12 @@
 import User from "../../models/User"
 import connectDb from "../../middleware/mongoose";
-
+var CryptoJS = require("crypto-js");
 
 const  handler = async (req,res)=>{
 
     if(req.method=="POST"){
         const {name,email}=(req.body)
-        let u = new User({name,email,password:" "})
+        let u = new User({name,email,password: CryptoJS.AES.encrypt(req.body.password,"secret123").toString()})
         await u.save()
         
         res.status(200).json({success: "success"})
